1cb944f49f0bd5bdf477d3909b18392dc412e2bc
Guillaume G <guillaume.garcera@vodeclic.com>
2017-04-12 19:18:12 +0200

    Make Table a Functional Stateless Component

--- src/App.js
+++ src/App.js
@@ -48,30 +48,25 @@ class Button extends Component {
   }
 }
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        ))}
+const Table = ({ list, pattern, onDismiss }) => (
+  <div>
+    {list.filter(isSearched(pattern)).map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)}>
+            Dismiss
+          </Button>
+        </span>
       </div>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
 class App extends Component {
   constructor(props) {
